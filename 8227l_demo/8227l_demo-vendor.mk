bc7a1ce3ad6ecf29f2cdebd28eff8c05b65a9964
Elenedeath <necromus.hikage@gmail.com>
2020-07-28 04:51:33 +0200

    testing stuff
    testing stuff

--- 8227l_demo/8227l_demo-vendor.mk
+++ 8227l_demo/8227l_demo-vendor.mk
@@ -1,54 +1,38 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-# The gps config appropriate for this device
-$(call inherit-product, device/common/gps/gps_us_supl.mk)
-
-$(call inherit-product-if-exists, vendor/alps/8227L_demo/8227L_demo-vendor.mk)
-
-DEVICE_PACKAGE_OVERLAYS += device/alps/8227L_demo/overlay
-
-$(call inherit-product, build/target/product/full.mk)
-
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-PRODUCT_NAME := full_8227L_demo
-PRODUCT_DEVICE := 8227L_demo
-
-PRODUCT_SOONG_NAMESPACES += \
-    vendor/alps/8227L_demo
+PRODUCT_COPY_FILES += \
+    vendor/alps/8227l_demo/proprietary/vendor/data/misc/ProjectConfig.mk:$(TARGET_COPY_OUT_VENDOR)/data/misc/ProjectConfig.mk \
+    vendor/alps/8227l_demo/proprietary/vendor/etc/mtk_omx_core.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/mtk_omx_core.cfg \
+    vendor/alps/8227l_demo/proprietary/vendor/lib/mediadrm/libdrmclearkeyplugin.so:$(TARGET_COPY_OUT_VENDOR)/lib/mediadrm/libdrmclearkeyplugin.so \
+    vendor/alps/8227l_demo/proprietary/vendor/lib/libClearMotionFW.so:$(TARGET_COPY_OUT_VENDOR)/lib/libClearMotionFW.so \
+    vendor/alps/8227l_demo/proprietary/vendor/lib/libdpframework.so:$(TARGET_COPY_OUT_VENDOR)/lib/libdpframework.so \
+    vendor/alps/8227l_demo/proprietary/vendor/lib/libmp2dec_sa.ca7.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmp2dec_sa.ca7.so \
+    vendor/alps/8227l_demo/proprietary/vendor/lib/libmp4enc_sa.ca7.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmp4enc_sa.ca7.so \
+    vendor/alps/8227l_demo/proprietary/vendor/lib/libMtkOmxCore.so:$(TARGET_COPY_OUT_VENDOR)/lib/libMtkOmxCore.so \
+    vendor/alps/8227l_demo/proprietary/vendor/lib/libMtkOmxG711Dec.so:$(TARGET_COPY_OUT_VENDOR)/lib/libMtkOmxG711Dec.so \
+    vendor/alps/8227l_demo/proprietary/vendor/lib/libMtkOmxGsmDec.so:$(TARGET_COPY_OUT_VENDOR)/lib/libMtkOmxGsmDec.so \
+    vendor/alps/8227l_demo/proprietary/vendor/lib/libMtkOmxMp3Dec.so:$(TARGET_COPY_OUT_VENDOR)/lib/libMtkOmxMp3Dec.so \
+    vendor/alps/8227l_demo/proprietary/vendor/lib/libMtkOmxRawDec.so:$(TARGET_COPY_OUT_VENDOR)/lib/libMtkOmxRawDec.so \
+    vendor/alps/8227l_demo/proprietary/vendor/lib/libMtkOmxVdecEx.so:$(TARGET_COPY_OUT_VENDOR)/lib/libMtkOmxVdecEx.so \
+    vendor/alps/8227l_demo/proprietary/vendor/lib/libMtkOmxVenc.so:$(TARGET_COPY_OUT_VENDOR)/lib/libMtkOmxVenc.so \
+    vendor/alps/8227l_demo/proprietary/vendor/lib/libMtkOmxVorbisEnc.so:$(TARGET_COPY_OUT_VENDOR)/lib/libMtkOmxVorbisEnc.so \
+    vendor/alps/8227l_demo/proprietary/vendor/lib/librrc.so:$(TARGET_COPY_OUT_VENDOR)/lib/librrc.so \
+    vendor/alps/8227l_demo/proprietary/vendor/lib/libvc1dec_sa.ca7.so:$(TARGET_COPY_OUT_VENDOR)/lib/libvc1dec_sa.ca7.so \
+    vendor/alps/8227l_demo/proprietary/vendor/lib/libvcodecdrv.so:$(TARGET_COPY_OUT_VENDOR)/lib/libvcodecdrv.so \
+    vendor/alps/8227l_demo/proprietary/vendor/lib/libvcodec_oal.so:$(TARGET_COPY_OUT_VENDOR)/lib/libvcodec_oal.so \
+    vendor/alps/8227l_demo/proprietary/vendor/lib/libvcodec_utility.so:$(TARGET_COPY_OUT_VENDOR)/lib/libvcodec_utility.so \
+    vendor/alps/8227l_demo/proprietary/vendor/lib/libvp8dec_sa.ca7.so:$(TARGET_COPY_OUT_VENDOR)/lib/libvp8dec_sa.ca7.so \
+    vendor/alps/8227l_demo/proprietary/vendor/lib/libvp8enc_sa.ca7.so:$(TARGET_COPY_OUT_VENDOR)/lib/libvp8enc_sa.ca7.so \
+    vendor/alps/8227l_demo/proprietary/vendor/lib/libvp9dec_sa.ca7.so:$(TARGET_COPY_OUT_VENDOR)/lib/libvp9dec_sa.ca7.so \
+    vendor/alps/8227l_demo/proprietary/vendor/media/LMspeed_508.emd:$(TARGET_COPY_OUT_VENDOR)/media/LMspeed_508.emd \
+    vendor/alps/8227l_demo/proprietary/vendor/media/PFFprec_600.emd:$(TARGET_COPY_OUT_VENDOR)/media/PFFprec_600.emd
 
 PRODUCT_COPY_FILES += \
-    vendor/alps/8227L_demo/proprietary/vendor/data/misc/ProjectConfig.mk:$(TARGET_COPY_OUT_VENDOR)/data/misc/ProjectConfig.mk \
-    vendor/alps/8227L_demo/proprietary/vendor/etc/mtk_omx_core.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/mtk_omx_core.cfg \
-    vendor/alps/8227L_demo/proprietary/vendor/lib/mediadrm/libdrmclearkeyplugin.so:$(TARGET_COPY_OUT_VENDOR)/lib/mediadrm/libdrmclearkeyplugin.so \
-    vendor/alps/8227L_demo/proprietary/vendor/lib/libClearMotionFW.so:$(TARGET_COPY_OUT_VENDOR)/lib/libClearMotionFW.so \
-    vendor/alps/8227L_demo/proprietary/vendor/lib/libdpframework.so:$(TARGET_COPY_OUT_VENDOR)/lib/libdpframework.so \
-    vendor/alps/8227L_demo/proprietary/vendor/lib/libmp2dec_sa.ca7.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmp2dec_sa.ca7.so \
-    vendor/alps/8227L_demo/proprietary/vendor/lib/libmp4enc_sa.ca7.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmp4enc_sa.ca7.so \
-    vendor/alps/8227L_demo/proprietary/vendor/lib/libMtkOmxCore.so:$(TARGET_COPY_OUT_VENDOR)/lib/libMtkOmxCore.so \
-    vendor/alps/8227L_demo/proprietary/vendor/lib/libMtkOmxG711Dec.so:$(TARGET_COPY_OUT_VENDOR)/lib/libMtkOmxG711Dec.so \
-    vendor/alps/8227L_demo/proprietary/vendor/lib/libMtkOmxGsmDec.so:$(TARGET_COPY_OUT_VENDOR)/lib/libMtkOmxGsmDec.so \
-    vendor/alps/8227L_demo/proprietary/vendor/lib/libMtkOmxMp3Dec.so:$(TARGET_COPY_OUT_VENDOR)/lib/libMtkOmxMp3Dec.so \
-    vendor/alps/8227L_demo/proprietary/vendor/lib/libMtkOmxRawDec.so:$(TARGET_COPY_OUT_VENDOR)/lib/libMtkOmxRawDec.so \
-    vendor/alps/8227L_demo/proprietary/vendor/lib/libMtkOmxVdecEx.so:$(TARGET_COPY_OUT_VENDOR)/lib/libMtkOmxVdecEx.so \
-    vendor/alps/8227L_demo/proprietary/vendor/lib/libMtkOmxVenc.so:$(TARGET_COPY_OUT_VENDOR)/lib/libMtkOmxVenc.so \
-    vendor/alps/8227L_demo/proprietary/vendor/lib/libMtkOmxVorbisEnc.so:$(TARGET_COPY_OUT_VENDOR)/lib/libMtkOmxVorbisEnc.so \
-    vendor/alps/8227L_demo/proprietary/vendor/lib/librrc.so:$(TARGET_COPY_OUT_VENDOR)/lib/librrc.so \
-    vendor/alps/8227L_demo/proprietary/vendor/lib/libvc1dec_sa.ca7.so:$(TARGET_COPY_OUT_VENDOR)/lib/libvc1dec_sa.ca7.so \
-    vendor/alps/8227L_demo/proprietary/vendor/lib/libvcodecdrv.so:$(TARGET_COPY_OUT_VENDOR)/lib/libvcodecdrv.so \
-    vendor/alps/8227L_demo/proprietary/vendor/lib/libvcodec_oal.so:$(TARGET_COPY_OUT_VENDOR)/lib/libvcodec_oal.so \
-    vendor/alps/8227L_demo/proprietary/vendor/lib/libvcodec_utility.so:$(TARGET_COPY_OUT_VENDOR)/lib/libvcodec_utility.so \
-    vendor/alps/8227L_demo/proprietary/vendor/lib/libvp8dec_sa.ca7.so:$(TARGET_COPY_OUT_VENDOR)/lib/libvp8dec_sa.ca7.so \
-    vendor/alps/8227L_demo/proprietary/vendor/lib/libvp8enc_sa.ca7.so:$(TARGET_COPY_OUT_VENDOR)/lib/libvp8enc_sa.ca7.so \
-    vendor/alps/8227L_demo/proprietary/vendor/lib/libvp9dec_sa.ca7.so:$(TARGET_COPY_OUT_VENDOR)/lib/libvp9dec_sa.ca7.so \
-    vendor/alps/8227L_demo/proprietary/vendor/media/LMspeed_508.emd:$(TARGET_COPY_OUT_VENDOR)/media/LMspeed_508.emd \
-    vendor/alps/8227L_demo/proprietary/vendor/media/PFFprec_600.emd:$(TARGET_COPY_OUT_VENDOR)/media/PFFprec_600.emd \
-    vendor/alps/8227L_demo/proprietary/bin/xyautocpnavi:$(TARGET_COPY_OUT_SYSTEM)/bin/xyautocpnavi \
-    vendor/alps/8227L_demo/proprietary/bin/xydelinsidefiles:$(TARGET_COPY_OUT_SYSTEM)/bin/xydelinsidefiles \
-    vendor/alps/8227L_demo/proprietary/bin/xyrminsidefiles:$(TARGET_COPY_OUT_SYSTEM)/bin/xyrminsidefiles \
-    vendor/alps/8227L_demo/proprietary/etc/init/xycpdata.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/xycpdata.rc \
-    vendor/alps/8227L_demo/proprietary/etc/init/xyremfile.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/xyremfile.rc \
-    vendor/alps/8227L_demo/proprietary/etc/clatd.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/clatd.conf \
-    vendor/alps/8227L_demo/proprietary/etc/custom.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/custom.conf \
-    vendor/alps/8227L_demo/proprietary/etc/factory.ini:$(TARGET_COPY_OUT_SYSTEM)/etc/factory.ini \
-    vendor/alps/8227L_demo/proprietary/etc/mtklog-config.prop:$(TARGET_COPY_OUT_SYSTEM)/etc/mtklog-config.prop
+    vendor/alps/8227l_demo/proprietary/bin/xyautocpnavi:$(TARGET_COPY_OUT_SYSTEM)/bin/xyautocpnavi \
+    vendor/alps/8227l_demo/proprietary/bin/xydelinsidefiles:$(TARGET_COPY_OUT_SYSTEM)/bin/xydelinsidefiles \
+    vendor/alps/8227l_demo/proprietary/bin/xyrminsidefiles:$(TARGET_COPY_OUT_SYSTEM)/bin/xyrminsidefiles \
+    vendor/alps/8227l_demo/proprietary/etc/init/xycpdata.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/xycpdata.rc \
+    vendor/alps/8227l_demo/proprietary/etc/init/xyremfile.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/xyremfile.rc \
+    vendor/alps/8227l_demo/proprietary/etc/clatd.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/clatd.conf \
+    vendor/alps/8227l_demo/proprietary/etc/custom.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/custom.conf \
+    vendor/alps/8227l_demo/proprietary/etc/factory.ini:$(TARGET_COPY_OUT_SYSTEM)/etc/factory.ini \
+    vendor/alps/8227l_demo/proprietary/etc/mtklog-config.prop:$(TARGET_COPY_OUT_SYSTEM)/etc/mtklog-config.prop
 
--- 8227l_demo/Android.mk
+++ 8227l_demo/Android.mk
@@ -19,3 +19,19 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),8227l_demo)
 
 endif
+
+# Release name
+PRODUCT_RELEASE_NAME := 8227l_demo
+
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/alps/8227l_demo/device_8227l_demo.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := 8227l_demo
+PRODUCT_NAME := lineage_8227l_demo
+PRODUCT_BRAND := alps
+PRODUCT_MODEL := 8227l_demo
+PRODUCT_MANUFACTURER := alps
